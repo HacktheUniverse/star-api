cc4dbf2347466ef05f1d3d40349efa741721f331
nichol alexander <nichol.alexander@gmail.com>
2014-11-08 07:55:49 -0500

    filters, but not multiple

--- app/controllers/API/v1/resources_controller.rb
+++ app/controllers/API/v1/resources_controller.rb
@@ -4,38 +4,45 @@ module API
       before_action :set_resource_class
       before_action :set_resource, only: [:show]
       respond_to :json
-
+ 
       def index
         items = @resource_class.all
-        render json: items
-      end
-
-      def show
         if request.query_string.present?
-          request.query_parameters.each do |k, v|
-            if k == 'max_distance_from_sun' && @resource_class == Star
-              @items = @resource_class.where('distly < ?', v)
-              # render json: items, count: items.count
-              render '/stars/query'
-            end
+          filters.each do |filter|
+            @items = items.where(filter)
           end
-        else
-          render :json => @item
         end
       end
-
-
-
-
+ 
+      def show
+        render :json => @item
+      end
+ 
+ 
       private
       def resource
         params[:resource]
       end
-
+ 
+      def filters  
+        max_filters.map {|key, value| "#{key} < #{value}"} + 
+        min_filters.map {|key, value| "#{key} > #{value}"}
+      end
+ 
+      def max_filters
+        params[:max] = [] if params[:max].blank?
+        params[:max].select {|key, value| @resource_class.column_names.include? key.to_s } if params[:max]
+      end
+ 
+      def min_filters
+        params[:min] = [] if params[:min].blank?
+        params[:min].select {|key, value| @resource_class.column_names.include? key.to_s }
+      end
+ 
       def set_resource
         @item = @resource_class.find_by_label(params[:id])
       end
-
+ 
       def set_resource_class
         @resource_class = resource.classify.constantize
       end
