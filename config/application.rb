47ad21bf673ff5139716cf365fd27f28ede3a3dd
jdmcpeek <david.mcpeek@yale.edu>
2014-11-08 07:45:45 -0500

    gzip

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,9 @@ Bundler.require(*Rails.groups)
 
 module StarApi
   class Application < Rails::Application
+
+    config.middleware.use Rack::Deflater
+    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
